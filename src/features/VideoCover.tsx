dfcdd93ea45d64d1229078ef709aa39343225c63
mlmar <marcloussm@gmail.com>
2026-03-18 13:34:02 -0700

    Styling video cover section

--- src/features/VideoCover.tsx
+++ src/features/VideoCover.tsx
@@ -1,14 +1,20 @@
+import styles from "./VideoCover.module.less";
+
 export function VideoCover() {
     return (
-        <section>
+        <section className={styles.videoCover}>
             <h2>
                 <span>We're still flying high on 2023 leftovers.</span>
+                <br />
                 <span>
                     Click the tube to relive some action from last year.
                 </span>
             </h2>
             <a href="https://google.com">
-                <img src="/video-cover.webp" alt="Video Cover" />
+                <figure>
+                    <img src="/video-cover.webp" alt="Video Cover" />
+                    <figcaption> View More on YouTube &#9654;</figcaption>
+                </figure>
             </a>
         </section>
     );
