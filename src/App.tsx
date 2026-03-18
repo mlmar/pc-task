ecd2b2185026177ab664ea7d3db556cbf623ec0e
mlmar <marcloussm@gmail.com>
2026-03-18 11:04:02 -0700

    Scaffolding video cover section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
+import { VideoCover } from "./components/VideoCover";
 
 function App() {
     return (
         <main>
             <Header />
             <Hero />
+            <VideoCover />
         </main>
     );
 }
---
+++ src/components/VideoCover.tsx
@@ -0,0 +1,15 @@
+export function VideoCover() {
+    return (
+        <section>
+            <h2>
+                <span>We're still flying high on 2023 leftovers.</span>
+                <span>
+                    Click the tube to relive some action from last year.
+                </span>
+            </h2>
+            <a href="https://google.com">
+                <img src="/video-cover.webp" alt="Video Cover" />
+            </a>
+        </section>
+    );
+}
